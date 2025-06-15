4772839f332dcb5e81edf2aa2add5c6ab9798e0e
guaxinim <75952546+silrenan@users.noreply.github.com>
2025-06-15 17:53:21 -0300

    Initial scaffold
    Signed-off-by: guaxinim <75952546+silrenan@users.noreply.github.com>

---
+++ src/main/java/io/github/silrenan/controller/Main.java
@@ -0,0 +1,10 @@
+package io.github.silrenan.controller;
+
+/**
+ * Hello world!
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
